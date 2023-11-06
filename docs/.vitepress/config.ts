9c2ad76518a58fab14028704ddd2bb9a895b9b91
ilcotvce <zhuxinliang666@outlook.com>
2023-11-06 15:31:59 +0800

    feature(): update.

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,28 +2,42 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "ICTC CMS",
-  description: "A CMS Stie",
+  title: "ICTC",
+  description: "弘扬中华优秀传统文化 实现中华民族伟大复兴",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
+    logo: "/images/logo.svg",
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: '首页', link: '/' },
+      { text: '关于', link: '/about' },
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: '目录',
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+          { text: '中国琴', link: '/ChineseQin/index.md' },
+          { text: '中国棋', link: '/ChineseQi/index.md' },
+          { text: '中国书法', link: '/ChineseShuFa/index.md' },
+          { text: '中国画', link: '/ChineseHua/index.md' },
         ]
       }
     ],
 
-    socialLinks: [
-      { icon: 'github', link: 'ilcotvce.github.io' }
-    ]
+    footer: {
+      message: '人生如梦，一尊还酹江月',
+      copyright: 'Cechnical Support © 2023-present Jiang Yue.'
+    },
+
+    // socialLinks: [
+    //   { icon: 'github', link: 'ilcotvce.github.io' }
+    // ]
   },
-  base: '/'
+  base: '/',
+  locales: {
+    root: {
+      label: '中文',
+      lang: 'zh-cn'
+    },
+  }
 })
--- docs/index.md
+++ docs/index.md
@@ -5,26 +5,43 @@ layout: home
 hero:
   name: "ICTC CMS"
   text: "A CMS Stie"
-  tagline: My great project tagline
+  tagline: 道可道，非常道
+  image:
+    src: /images/logo.svg
+    alt: 道
   actions:
     - theme: brand
-      text: Markdown Examples
-      link: /markdown-examples
+      text: 开始探索
+      link: /start
     - theme: alt
-      text: API Examples
-      link: /api-examples
+      text: 关于
+      link: /about
 
 features:
-  - title: Feature A
+  - icon:
+      src: /icons/pipa.svg
+    title: 中国琴
     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature B
+    link: /ChineseQin/index.md
+  - icon:
+      src: /icons/weiqi.svg
+    title: 中国棋
     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature C
+    link: /ChineseQi/index.md
+  - icon:
+      src: /icons/shufa.svg
+    title: 中国书法
     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+    link: /ChineseShuFa/index.md
+  - icon:
+      src: /icons/huihua.svg
+    title: 中国画
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+    link: /ChineseHua/index.md
 ---
-<script setup>
+
+<!-- <script setup>
   import HelloWorld from './components/HelloWorld.vue';
-</script>
+</script> -->
 
 <!-- <HelloWorld></HelloWorld> -->
-
